440d02992ce2eb17a41546a116754930c57fadcd
k-rdo15 <ricardo.figueiredodc@gmail.com>
2019-11-27 19:16:05 +0100

    Ajout de champs creationJoueur

--- lib/screens/CreerJoueur.dart
+++ lib/screens/CreerJoueur.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class creerJoueur extends StatefulWidget {
   @override
@@ -30,17 +31,66 @@ class customForm extends StatefulWidget {
 final _formKey = GlobalKey<FormState>();
 
 class _customFormState extends State<customForm> {
-  @override
+   @override
   Widget build(BuildContext context) {
     return Form(
       key: _formKey,
-      child: TextFormField(
-        validator: (value) {
-        if (value.isEmpty) {
-          return 'Please enter some text';
-        }
-        return null;
-        },
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextFormField(
+            decoration: InputDecoration(
+              labelText: 'Prénom'
+            ),
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Veuillez remplir le champ';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+              labelText: 'Nom'
+            ),
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Veuillez remplir le champ';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            keyboardType: TextInputType.number,
+            inputFormatters: <TextInputFormatter>[
+        WhitelistingTextInputFormatter.digitsOnly
+    ],
+            decoration: InputDecoration(
+              labelText: 'Numero'
+            ),
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Veuillez remplir le champ';
+              }
+              return null;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: RaisedButton(
+              onPressed: () {
+                // Validate returns true if the form is valid, or false
+                // otherwise.
+                if (_formKey.currentState.validate()) {
+                  // If the form is valid, display a Snackbar.
+                  Scaffold.of(context)
+                      .showSnackBar(SnackBar(content: Text('Joueur ajouté')));
+                }
+              },
+              child: Text('Submit'),
+            ),
+          ),
+        ],
       ),
     );
   }
