d19096b7320ea493fad082f574baafd13d8ec464
k-rdo15 <ricardo.figueiredodc@gmail.com>
2019-12-02 18:11:29 +0100

    Add comments

--- lib/models/Match.dart
+++ lib/models/Match.dart
@@ -10,4 +10,8 @@ class Match{
       this.adversaryGoals = adversaryGoals;
       this.teamGoals = teamGoals;
     }
+
+    //vu qu'un match est composé de titulaires et de remplacants je sais pas si on doit faire une List de joueurs pour ce match
+    // genre si on a une équipe de 20 joueurs mais qu'on ne peut qu'en mettre 12 il faudra choisir et pas tous les joueurs auront participé
+    // au match, si t'as pas trop saisi le sens dis moi je t'expliquerais.
 }
