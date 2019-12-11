46fdf7621a322d4826151950b39dde61b9e2d075
k-rdo15 <ricardo.figueiredodc@gmail.com>
2019-12-11 18:48:31 +0100

    Modification design création joueur

--- lib/screens/CreerJoueur.dart
+++ lib/screens/CreerJoueur.dart
@@ -12,13 +12,16 @@ class _creerJoueurState extends State<creerJoueur> {
   @override
   Widget build(BuildContext context) {
      return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Colors.black,
-          title: Text('Création de joueur', 
-          style: TextStyle(
-            color: Colors.yellow ),
-         ),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(80.0),
+          child: AppBar(
+            backgroundColor: Color(0xff0D0043),
+            title: Text('Création de joueur', 
+            style: TextStyle(
+              fontSize: 25.0,
+              color: Colors.white ),
+           ),
+        ),
       ),
       body: customForm()
       );
@@ -43,7 +46,9 @@ class _customFormState extends State<customForm> {
     Joueur joueur =  new Joueur(_number, _name, _firstName);
     
     print(_name);
-    print(joueur.numero);
+    print(_firstName);
+    print(_number);
+    print(_post);
   }
 
    @override
@@ -53,15 +58,27 @@ class _customFormState extends State<customForm> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
+          Icon(
+            Icons.person_add,
+            size: 100.0,
+            color: Color(0xff0D0043),
+          ),
           Padding(
             padding: const EdgeInsets.only(top: 20.0, left: 20, right: 20),
               child: TextFormField(
               autofocus: true,
               decoration: InputDecoration(
+                enabledBorder: const OutlineInputBorder(
+                  borderRadius: const BorderRadius.all(
+                    const Radius.circular(10.0),
+                  ),
+                  borderSide: const BorderSide(color: Color(0xff0D0043), width: 1.0),
+                ),
                  border: OutlineInputBorder(),
                 labelText: 'Prénom'
               ),
               validator: (value) {
+                _name = value;
                 if (value.isEmpty) {
                   return 'Veuillez remplir le champ';
                 }
@@ -72,13 +89,20 @@ class _customFormState extends State<customForm> {
           ),
           Padding(
             padding: const EdgeInsets.only(top: 20, left: 20, right: 20),
-                      child: TextFormField(
+              child: TextFormField(
               decoration: InputDecoration(
-                border: OutlineInputBorder(),
+                enabledBorder: const OutlineInputBorder(
+                  borderRadius: const BorderRadius.all(
+                    const Radius.circular(10.0),
+                  ),
+                  borderSide: const BorderSide(color: Color(0xff0D0043), width: 1.0),
+                ),
+                border: OutlineInputBorder(
+                ),
                 labelText: 'Nom'
               ),
               validator: (value) {
-                _name = value;
+                _firstName = value;
                 if (value.isEmpty) {
                   return 'Veuillez remplir le champ';
                 }
@@ -89,17 +113,24 @@ class _customFormState extends State<customForm> {
           ),
           Padding(
             padding: const EdgeInsets.only(top: 20, left: 20, right: 20),
-                      child: TextFormField(
-              keyboardType: TextInputType.number, //Affiche le clavier numérique
-              inputFormatters: <TextInputFormatter>[
-                new LengthLimitingTextInputFormatter(2), // limitation des maillots de 0 à 99
-                WhitelistingTextInputFormatter.digitsOnly // force l'usage du clavier numérique
+              child: TextFormField(
+                keyboardType: TextInputType.number, //Affiche le clavier numérique
+                inputFormatters: <TextInputFormatter>[
+                  new LengthLimitingTextInputFormatter(2), // limitation des maillots de 0 à 99
+                  WhitelistingTextInputFormatter.digitsOnly // force l'usage du clavier numérique
               ],
               decoration: InputDecoration(
+                enabledBorder: const OutlineInputBorder(
+                  borderRadius: const BorderRadius.all(
+                    const Radius.circular(10.0),
+                  ),
+                  borderSide: const BorderSide(color: Color(0xff0D0043), width: 1.0),
+                ),
                  border: OutlineInputBorder(),
                 labelText: 'Numero'
               ),
               validator: (value) {
+                _number = int.parse(value);
                 if (value.isEmpty) {
                   return 'Veuillez remplir le champ';
                 }
@@ -146,17 +177,29 @@ class _customFormState extends State<customForm> {
                   valueField: 'value',
                 ),
               ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: RaisedButton(
-              onPressed: () {
-                if (_formKey.currentState.validate()) { //Verification de la validité de la Form
-                   ajouterJoueur();
-                  Scaffold.of(context)
-                      .showSnackBar(SnackBar(content: Text('Joueur ajouté')));
-                }
-              },
-              child: Text('Créer'),
+          SizedBox(
+            width: 200,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: RaisedButton(
+                 shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(18.0),
+                  ),
+                color: Color(0xff281280),
+                onPressed: () {
+                  if (_formKey.currentState.validate()) { //Verification de la validité de la Form
+                     ajouterJoueur();
+                    Scaffold.of(context)
+                        .showSnackBar(SnackBar(content: Text('Joueur ajouté')));
+                  }
+                },
+                child: Text(
+                  'Créer',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
             ),
           ),
         ],
